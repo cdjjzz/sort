9dcd7073ad44e9ff456c9720d84071851b683962
pet-lsf <pet-lsf@LAPTOP-RKULFE8M>
2017-08-04 23:19:57 +0800

    啊实打实

---
+++ sort/src/com/sort/InsertSort.java
@@ -0,0 +1,20 @@
+package com.sort;
+
+import java.lang.reflect.Array;
+
+/**
+ * ≤Â»Î≈≈–Ú
+ * @author pet-lsf
+ *
+ */
+public class InsertSort {
+	
+	
+	
+	
+	public static void  print(Object object){
+			
+		
+	}
+	
+}
