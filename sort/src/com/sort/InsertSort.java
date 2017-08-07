b14acc42b1054d184e391c7eb71bc042732b8d8b
pet-lsf <pet-lsf@LAPTOP-RKULFE8M>
2017-08-07 10:57:50 +0800

    342

--- sort/src/com/sort/InsertSort.java
+++ sort/src/com/sort/InsertSort.java
@@ -1,18 +1,34 @@
 package com.sort;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+
 
 /**
- * ≤Â»Î≈≈–Ú
  * @author pet-lsf
  *
  */
 public class InsertSort {
 	
+	private static int[] sortSrc={1,2,3};
+	
+	public static void main(String[] args) {
+		print(sortSrc);
+		
+	}
+	
+	
+	public static void sort(){
+	}
 	
 	
 	
-	public static void  print(Object object){
-			
+	
+	public static  void  print(int[] sortSrc){
+		for (int i = 0; i <sortSrc.length; i++) {
+			System.out.println(sortSrc[i]);
+		}
 		
 	}
 	
