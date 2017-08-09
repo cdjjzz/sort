ca0f5e38a38f11a9e12e9fcec3811d2e597efe6b
pet-lsf <pet-lsf@LAPTOP-RKULFE8M>
2017-08-09 17:53:02 +0800

    插入排序

--- sort/src/com/sort/InsertSort.java
+++ sort/src/com/sort/InsertSort.java
@@ -1,8 +1,5 @@
 package com.sort;
 
-import java.lang.reflect.Array;
-import java.util.Arrays;
-import java.util.List;
 
 
 /**
@@ -11,15 +8,44 @@ import java.util.List;
  */
 public class InsertSort {
 	
-	private static int[] sortSrc={1,2,3};
+	private static int[] sortSrc={4444,3333,2222,1111};
 	
 	public static void main(String[] args) {
+		print(sortSrc);
+		sort();
 		print(sortSrc);
 		
 	}
 	
-	
+	/**
+	 * 稳定 
+	 *	空间复杂度O(1) 
+	 *	时间复杂度O(n2) 
+	 *	最差情况：反序，需要移动n*(n-1)/2个元素 
+	 *	最好情况：正序，不需要移动元素
+	 * @Title: sort 
+	 * @Description: TODO
+	 * @param 
+	 * @return void 
+	 * @throws
+	 */
 	public static void sort(){
+		int count=0;
+		for(int i=1;i<sortSrc.length;i++){
+			boolean flag=false;
+			int temp=sortSrc[i];
+			int j=i;
+			while(j>0&&sortSrc[j-1]>temp){
+				sortSrc[j]=sortSrc[j-1];
+				j--;
+				count++;
+				flag=true;
+			}
+			if(!flag)
+			count++;
+			sortSrc[j]=temp;
+		}
+		System.out.println(count);
 	}
 	
 	
@@ -27,8 +53,9 @@ public class InsertSort {
 	
 	public static  void  print(int[] sortSrc){
 		for (int i = 0; i <sortSrc.length; i++) {
-			System.out.println(sortSrc[i]);
+			System.out.print(sortSrc[i]+"\t");
 		}
+		System.out.println();
 		
 	}
 	
